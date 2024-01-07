9bf76135d3dede36c563245272fd426aba0074b2
ganajayant <ganajayant28@gmail.com>
2024-01-08 00:07:07 +0530

    updated lsp

--- nvim/lua/ganajayant/plugins-setup.lua
+++ nvim/lua/ganajayant/plugins-setup.lua
@@ -58,6 +58,7 @@ return packer.startup(function(use)
   use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
   use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-  if packer_bootstrap then require('packer').sync()
+  if packer_bootstrap then 
+      require('packer').sync()
   end
 end)
