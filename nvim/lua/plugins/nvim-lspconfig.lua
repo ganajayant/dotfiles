27c99754db280a0985abfbd36fb0eca076abf4a1
ganajayant <ganajayant28@gmail.com>
2025-05-21 13:07:59 +0530

    feat(aerospace): changed layout

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -53,6 +53,9 @@ return {
 				--  For example, in C this would take you to the header.
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+				map("K", vim.lsp.buf.hover, "Show hover information")
+				map("<C-k>", vim.lsp.buf.signature_help, "Show signature help")
+
 				-- The following two autocommands are used to highlight references of the
 				-- word under your cursor when your cursor rests there for a little while.
 				--    See `:help CursorHold` for information about when this is executed
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,6 +3,10 @@ return {
 	cmd = "Trouble",
 	opts = {},
 	keys = {
-		{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle focus=true<cr>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics" },
+		{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics" },
+		{ "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List" },
+		{ "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List" },
 	},
 }
