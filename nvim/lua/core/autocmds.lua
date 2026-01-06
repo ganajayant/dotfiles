727a0dde657d94ce1de8d39d08023704b8b8adb3
ganajayant <ganajayant28@gmail.com>
2026-01-06 12:06:38 +0530

    fix(treesitter)

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -13,15 +13,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	command = [[%s/\s\+$//e]],
 })
 
-vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
-	pattern = { "docker-compose.yaml", "compose.yaml" },
-	group = vim.api.nvim_create_augroup("ft_lsp_group", { clear = true }),
-	desc = "Fix the issue where the LSP does not start with docker-compose.",
-	callback = function()
-		vim.opt.filetype = "yaml.docker-compose"
-	end,
-})
-
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		if vim.fn.expand("%") == "" then
--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,27 +1,31 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	lazy = false,
-	branch = "main",
 	build = ":TSUpdate",
-	init = function()
-		vim.defer_fn(function()
-			require("nvim-treesitter").install({
-				"bash",
-				"c",
-				"cpp",
-				"diff",
-				"html",
-				"java",
-				"lua",
-				"luadoc",
-				"markdown",
-				"toml",
-				"vim",
-				"vimdoc",
-				"yaml",
-			})
-		end, 1000)
-		require("nvim-treesitter").update()
+	config = function()
+		require("nvim-treesitter").setup({})
+		local langs = {
+			"bash",
+			"c",
+			"cpp",
+			"diff",
+			"html",
+			"java",
+			"lua",
+			"luadoc",
+			"markdown",
+			"toml",
+			"vim",
+			"vimdoc",
+			"yaml",
+		}
+		require("nvim-treesitter").install(langs)
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				pcall(vim.treesitter.start)
+			end,
+		})
+
 		vim.api.nvim_create_autocmd("FileType", {
 			desc = "User: enable treesitter highlighting",
 			callback = function(ctx)
@@ -30,36 +34,6 @@ return {
 				if hasStarted and not vim.list_contains(noIndent, ctx.match) then
 					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 				end
-			end,
-		})
-		vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-			desc = "Auto-install treesitter parsers on buffer enter",
-			callback = function(event)
-				local bufnr = event.buf
-				local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
-				if filetype == "" then
-					return
-				end
-				local parser_name = vim.treesitter.language.get_lang(filetype)
-				if not parser_name then
-					return
-				end
-				local parser_configs = require("nvim-treesitter.parsers")
-				local parser_can_be_used = parser_configs[parser_name]
-				if not parser_can_be_used then
-					return
-				end
-				local parser_installed = pcall(vim.treesitter.get_parser, bufnr, parser_name)
-				if not parser_installed then
-					vim.defer_fn(function()
-						require("nvim-treesitter").install({ parser_name })
-						vim.defer_fn(function()
-							pcall(vim.treesitter.start, bufnr, parser_name)
-						end, 500)
-					end, 0)
-					return
-				end
-				vim.treesitter.start(bufnr, parser_name)
 			end,
 		})
 	end,
