81b8c4d25d1e4675226226033abde02748a84c8c
ganajayant <ganajayant28@gmail.com>
2026-09-05 00:16:01 +0530

    fix(fzf): fzf pagination issues

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -12,7 +12,30 @@ export CLICOLOR=1
 export GIT_EDITOR="nvim"
 
 # FZF Preview
-export show_file_or_dir_preview="if [ -d {} ]; then ls -lha {} | head -200; else cat -n --color=always --line-range :500 {}; fi"
-export FZF_CTRL_T_OPTS="--preview \"$show_file_or_dir_preview\""
-export FZF_ALT_C_OPTS="--preview 'ls -lha {} | head -200'"
+export show_file_or_dir_preview='
+  if [ -d "{}" ]; then
+    ls -lha "{}" | head -200
+  else
+    cat -n --color=always --line-range :500 "{}"
+  fi
+'
+
+# File finder
+export FZF_CTRL_T_OPTS="
+  --preview '$show_file_or_dir_preview'
+  --preview-window='right:60%:border-left'
+"
+
+# General FZF
+export FZF_DEFAULT_OPTS="
+  --height=60%
+  --layout=reverse
+  --border=rounded
+  --info=inline
+  --prompt='❯ '
+  --cycle
+  --scroll-off=5
+  --bind='ctrl-j:down'
+  --bind='ctrl-k:up'
+"
 export XDG_RUNTIME_DIR="${TMPDIR:-/tmp}"
